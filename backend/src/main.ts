1b86b58ab79a17701d77f8fdf08eb863a868b7cc
PaperPlane01 <dagfortnewage@gmail.com>
2020-02-19 16:45:38 +0600

    fix: (19.02.20) Fixed pagination parameters were not valdiated properly

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import {config} from "dotenv";
 config();
 
 import {NestFactory} from "@nestjs/core";
+import {ValidationPipe} from "@nestjs/common";
 import bodyParser from "body-parser";
 import {AppModule} from "./AppModule";
 import {config as envConfig} from "./config";
@@ -9,6 +10,7 @@ import {config as envConfig} from "./config";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({limit: Infinity}));
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(envConfig.DATA_VALIDATOR_API_PORT);
 }
--- backend/src/utils/pagination.ts
+++ backend/src/utils/pagination.ts
@@ -1,4 +1,5 @@
 import {IsInt, IsPositive} from "class-validator";
+import {Transform} from "class-transformer";
 
 export const getValidPage = (page?: string | number, fallback: number = 1, startFromZero: boolean = false): number => {
     let returnedPage = Number(page);
@@ -31,10 +32,12 @@ export const calculateOffset = (page: number, pageSize: number): number => (page
 export class PaginationRequest {
     @IsInt({message: "Page must be integer number"})
     @IsPositive({message: "Page must be positive"})
+    @Transform(value => Number(value))
     page: number = 1;
 
     @IsInt({message: "Page size must be integer number"})
     @IsPositive({message: "Page size must be positive"})
+    @Transform(value => Number(value))
     pageSize: number = 30;
 
     constructor(page: number = 1, pageSize: number = 30) {
