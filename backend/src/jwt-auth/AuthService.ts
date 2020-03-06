4d6c7dd9d489e0cd72f98c807ab5d502fb548ff9
PaperPlane01 <dagfortnewage@gmail.com>
2020-03-06 18:18:47 +0600

    feat: (06.02.20) Added logging of log in attempts and errors in case of invalid credentials

--- backend/src/jwt-auth/AuthService.ts
+++ backend/src/jwt-auth/AuthService.ts
@@ -1,5 +1,6 @@
-import {Injectable} from "@nestjs/common";
+import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
 import {JwtService} from "@nestjs/jwt";
+import {LoggerService} from "nest-logger";
 import {UsersService} from "../users";
 import {User} from "../users/entities";
 import {BCryptPasswordEncoder} from "../bcrypt";
@@ -10,19 +11,30 @@ export class AuthService {
     constructor(private readonly usersService: UsersService,
                 private readonly bCryptPasswordEncoder: BCryptPasswordEncoder,
                 private readonly jwtService: JwtService,
-                private readonly usersMapper: UsersMapper) {
+                private readonly usersMapper: UsersMapper,
+                private readonly log: LoggerService) {
     }
 
     public async validateUser(username: string, password: string): Promise<User | null> {
         try {
+            this.log.info(`Login attempt with the following username: ${username}`);
             const user = await this.usersService.findUserEntityByEthereumAddress(username);
 
             if (this.bCryptPasswordEncoder.matches(password, user.privateKey)) {
                 return user;
             }
 
+            this.log.info(`Invalid password has been provided for ${username}, bCryptPasswordEncoder returned false`);
+
             return null;
         } catch (error) {
+            if (error instanceof HttpException && error.getStatus() === HttpStatus.NOT_FOUND) {
+                this.log.info(`No user has been found with the following username: ${username}`)
+            } else {
+                this.log.error("Unexpected error occurred upon login attempt");
+                console.error(error);
+            }
+
             return null;
         }
     }
