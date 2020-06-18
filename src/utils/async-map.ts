35ee47bdb1b31ae9f582c054ccbef325e02d5093
PaperPlane01 <dagfortnewage@gmail.com>
2020-06-18 23:36:24 +0600

    fix: (18.06.20) Fixed async-map method

--- src/utils/async-map.ts
+++ src/utils/async-map.ts
@@ -1,7 +1,12 @@
 import {IteratingAsyncMappingCallback} from "./internal/callback-types";
 
 export const asyncMap = async <S, T>(array: S[], callback: IteratingAsyncMappingCallback<S, T>): Promise<T[]> => {
-    return Promise.all(
-        array.map(async (item, index, sourceArray) => await callback(item, index, sourceArray))
-    )
+    const result: T[] = [];
+
+    for (let index = 0; index < array.length; index++) {
+        const resultItem = await callback(array[index], index, array);
+        result.push(resultItem);
+    }
+
+    return result;
 };
