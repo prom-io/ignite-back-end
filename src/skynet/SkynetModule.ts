881db73d15224766c9811fee0d82c5b489832a93
PaperPlane01 <dagfortnewage@gmail.com>
2020-07-02 12:23:49 +0600

    fix: (02.07.20) Fixed uploading images to SIA

--- src/skynet/SkynetModule.ts
+++ src/skynet/SkynetModule.ts
@@ -1,17 +1,9 @@
 import {Module} from "@nestjs/common";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {ScheduleModule} from "nest-schedule";
 import {SkynetClient} from "./SkynetClient";
-import {SkynetImagesScheduledUploader} from "./SkynetImagesScheduledUploader";
-import {MediaAttachmentsRepository} from "../media-attachments/MediaAttachmentsRepository";
 
 @Module({
-    providers: [SkynetClient, SkynetImagesScheduledUploader],
-    exports: [SkynetClient],
-    imports: [
-        TypeOrmModule.forFeature([MediaAttachmentsRepository]),
-        ScheduleModule.register()
-    ]
+    providers: [SkynetClient],
+    exports: [SkynetClient]
 })
 export class SkynetModule {
 }
