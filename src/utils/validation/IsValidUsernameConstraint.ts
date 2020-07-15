d79b6890bba4ef3d2cb788ed85c32ea42de32295
PaperPlane01 <dagfortnewage@gmail.com>
2020-07-15 14:54:07 +0600

    fix: (15.07.20) Fixed username regexp as it allowed too many unwanted characters

--- src/utils/validation/IsValidUsernameConstraint.ts
+++ src/utils/validation/IsValidUsernameConstraint.ts
@@ -5,7 +5,7 @@ import {ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface} 
     async: false
 })
 export class IsValidUsernameConstraint implements ValidatorConstraintInterface {
-    private static readonly USERNAME_REGEXP = /^[a-zA-Z0-9\u3130-\u318F\uAC00-\uD7AF\u4E00-\u9FFF\u3400-\u4DBF\u20000-\u2A6DF\u2A700-\u2B73F\u2B740-\u2B81F\u2B820-\u2CEAF\uF900-\uFAFF\u2F800-\u2FA1F_]+$/;
+    private static readonly USERNAME_REGEXP = /^[a-zA-Z0-9_\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF\u4e00-\u9eff]+$/;
 
     public defaultMessage(validationArguments?: ValidationArguments): string {
         return "Username contains invalid characters";
