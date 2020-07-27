c08f51d8703862633785f7c8b4d022c01e19f9c2
Edgar <starguitar307@gmail.com>
2020-07-27 21:24:19 +0200

    refactor: use async/await in MediaAttachmentsService

--- src/media-attachments/MediaAttachmentsService.ts
+++ src/media-attachments/MediaAttachmentsService.ts
@@ -1,9 +1,9 @@
-import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
+import {HttpException, HttpStatus, Injectable, BadRequestException} from "@nestjs/common";
 import {LoggerService} from "nest-logger";
 import {Response} from "express";
 import fileSystem from "fs";
 import path from "path";
-import graphicsMagic from "gm";
+import graphicsMagic, { Dimensions } from "gm";
 import FileTypeExtractor from "file-type";
 import uuid from "uuid/v4";
 import {MediaAttachmentsRepository} from "./MediaAttachmentsRepository";
@@ -12,6 +12,8 @@ import {MediaAttachment} from "./entities";
 import {MediaAttachmentsMapper} from "./MediaAttachmentsMapper";
 import {config} from "../config";
 import {SkynetClient} from "../skynet";
+import {exists} from "../utils/file-utils"
+import {promisify} from "util"
 
 const gm = graphicsMagic.subClass({imageMagick: true});
 
@@ -23,70 +25,58 @@ export class MediaAttachmentsService {
                 private readonly log: LoggerService) {
     }
 
-    public saveMediaAttachment(multipartFile: MultipartFile): Promise<MediaAttachmentResponse> {
-        return new Promise<MediaAttachmentResponse>(async (resolve, reject) => {
-            try {
-                const id = uuid();
-                const temporaryFilePath = path.join(config.MEDIA_ATTACHMENTS_DIRECTORY, `${id}.temporary`);
-                const fileDescriptor = fileSystem.openSync(temporaryFilePath, "w");
-                fileSystem.writeSync(fileDescriptor, multipartFile.buffer);
-                fileSystem.closeSync(fileDescriptor);
+    public async saveMediaAttachment(multipartFile: MultipartFile): Promise<MediaAttachmentResponse> {
+        const id = uuid();
+        const temporaryFilePath = path.join(config.MEDIA_ATTACHMENTS_DIRECTORY, `${id}.temporary`);
+        await fileSystem.promises.writeFile(temporaryFilePath, multipartFile.buffer)
 
-                const file = await FileTypeExtractor.fromBuffer(fileSystem.readFileSync(temporaryFilePath));
-                if (file.mime.startsWith("image")) {
-                    gm(temporaryFilePath)
-                        .size(async (error, size) => {
-                            if (error) {
-                                console.log(error);
-                                reject(error);
-                            }
+        const fileTypeResult = await FileTypeExtractor.fromBuffer(multipartFile.buffer);
 
-                            const permanentFilePath = path.join(config.MEDIA_ATTACHMENTS_DIRECTORY, `${id}.${file.ext}`);
-                            fileSystem.renameSync(temporaryFilePath, permanentFilePath);
+        if (!fileTypeResult.mime.startsWith("image")) {
+            throw new BadRequestException("file is not an image");
+        }
 
-                            const mediaAttachment: MediaAttachment = {
-                                id,
-                                format: file.ext,
-                                mimeType: file.mime,
-                                height: size.height,
-                                width: size.width,
-                                name: `${id}.${file.ext}`,
-                                siaLink: null
-                            };
-                            await this.mediaAttachmentRepository.save(mediaAttachment);
+        const size = await this.getImageSize(temporaryFilePath)
 
-                            if (config.ENABLE_UPLOADING_IMAGES_TO_SIA) {
-                                this.skynetClient.uploadFile(permanentFilePath)
-                                    .then(async siaLink => {
-                                        this.log.info(`Media attachment ${mediaAttachment.name} has been uploaded to SIA`);
-                                        this.log.debug(`Media attachment ${mediaAttachment.name} received ${siaLink} SIA link`);
-                                        mediaAttachment.siaLink = siaLink;
-                                        await this.mediaAttachmentRepository.save(mediaAttachment);
-                                    })
-                                    .catch(error => {
-                                        this.log.error(`Error occurred when tried to upload media attachment ${mediaAttachment.name} to SIA`);
-                                        console.log(error);
-                                    });
-                            }
+        const permanentFilePath = path.join(config.MEDIA_ATTACHMENTS_DIRECTORY, `${id}.${fileTypeResult.ext}`);
+        await fileSystem.promises.rename(temporaryFilePath, permanentFilePath)
 
-                            resolve(this.mediaAttachmentsMapper.toMediaAttachmentResponse(mediaAttachment));
-                        })
-                }
-            } catch (error) {
-                console.log(error);
-                reject(error);
-            }
-        })
+        const mediaAttachment: MediaAttachment = {
+            id,
+            format: fileTypeResult.ext,
+            mimeType: fileTypeResult.mime,
+            height: size.height,
+            width: size.width,
+            name: `${id}.${fileTypeResult.ext}`,
+            siaLink: null
+        };
+        await this.mediaAttachmentRepository.save(mediaAttachment);
+
+        if (config.ENABLE_UPLOADING_IMAGES_TO_SIA) {
+            // do not await, cuz the execution can take several seconds
+            this.skynetClient.uploadFile(permanentFilePath)
+                .then(async siaLink => {
+                    this.log.info(`Media attachment ${mediaAttachment.name} has been uploaded to SIA`);
+                    this.log.debug(`Media attachment ${mediaAttachment.name} received ${siaLink} SIA link`);
+                    mediaAttachment.siaLink = siaLink;
+                    await this.mediaAttachmentRepository.save(mediaAttachment);
+                })
+                .catch(error => {
+                    this.log.error(`Error occurred when tried to upload media attachment ${mediaAttachment.name} to SIA`);
+                    console.log(error);
+                });
+        }
+
+        return this.mediaAttachmentsMapper.toMediaAttachmentResponse(mediaAttachment);
     }
 
     public async getMediaAttachmentByName(name: string, response: Response): Promise<void> {
         const mediaAttachment = await this.mediaAttachmentRepository.findByName(name);
 
         if (!mediaAttachment) {
-            throw new HttpException(`Could not find ${name}`, HttpStatus.NOT_FOUND);
-        }
+            throw new HttpException(`Could not find ${name}`, HttpStatus.NOT_FOUND)   }
 
-        const fileExists = fileSystem.existsSync(path.join(config.MEDIA_ATTACHMENTS_DIRECTORY, name));
+        const fileExists = await exists(path.join(config.MEDIA_ATTACHMENTS_DIRECTORY, name));
 
         if (fileExists) {
             response.header("Content-Type", mediaAttachment.mimeType);
@@ -96,5 +86,11 @@ export class MediaAttachmentsService {
             response.header("Content-Type", mediaAttachment.mimeType);
             fileSystem.createReadStream(path.join(config.MEDIA_ATTACHMENTS_DIRECTORY, name)).pipe(response);
         }
+    }
+
+    private async getImageSize(imagePath: string): Promise<Dimensions> {
+        const gmState = gm(imagePath)
+
+        return promisify(gmState.size.bind(gmState))()
     }
 }
--- src/utils/file-utils.ts
+++ src/utils/file-utils.ts
@@ -1,7 +1,12 @@
 import {lookup} from "mime-types";
+import fileSystem, { PathLike } from "fs"
 
 export const isImage = (fileExtension: string): boolean => {
     const mime = lookup(fileExtension);
 
     return typeof mime === "string" && mime.startsWith("image");
 };
+
+export const exists = (path: PathLike): Promise<boolean> => {
+    return fileSystem.promises.stat(path).then(() => true).catch(() => false)
+}
