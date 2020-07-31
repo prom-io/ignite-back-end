ffca59a47db4f607d062ff22139f5ee755e942f4
Edgar <starguitar307@gmail.com>
2020-07-31 21:54:51 +0200

    feat(swagger): Add Swagger endpoint

---
+++ src/setup-swagger.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle("Ignite Backend API")
+    .setDescription("Ignite Backend API")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup("swagger", app, document);
+}
