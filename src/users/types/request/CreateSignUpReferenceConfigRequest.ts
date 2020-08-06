c46832f2de22a24e3a10dded83bf790aabefa98d
Edgar <starguitar307@gmail.com>
2020-08-06 02:26:00 +0200

    docs(sign-up references): add creation request's body schema

--- src/users/types/request/CreateSignUpReferenceConfigRequest.ts
+++ src/users/types/request/CreateSignUpReferenceConfigRequest.ts
@@ -1,13 +1,16 @@
 import {IsArray, IsString, IsOptional} from "class-validator";
 import {ISignUpReferenceConfig} from "../ISignUpReferenceConfig";
 import {Expose} from "class-transformer";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 
 export class CreateSignUpReferenceConfigRequest implements ISignUpReferenceConfig {
+    @ApiProperty({ name: "accounts_to_subscribe", type: [String] })
     @Expose({name: "accounts_to_subscribe"})
     @IsArray()
     @IsString({each: true})
     accountsToSubscribe: string[];
 
+    @ApiPropertyOptional({ name: "accounts_to_recommend", type: [String] })
     @Expose({name: "accounts_to_recommend"})
     @IsArray()
     @IsString({each: true})
--- src/users/types/request/CreateSignUpReferenceRequest.ts
+++ src/users/types/request/CreateSignUpReferenceRequest.ts
@@ -1,19 +1,23 @@
 import {IsDateString, IsNumber, IsObject, ValidateIf, ValidateNested} from "class-validator";
 import {Expose, Transform} from "class-transformer";
 import {CreateSignUpReferenceConfigRequest} from "./CreateSignUpReferenceConfigRequest";
+import { ApiPropertyOptional, ApiProperty } from "@nestjs/swagger";
 
 export class CreateSignUpReferenceRequest {
+    @ApiPropertyOptional({ name: "expires_at", type: "string", format: "datetime" })
     @Expose({name: "expires_at"})
     @ValidateIf((object: CreateSignUpReferenceRequest) => Boolean(object.expiresAt))
     @IsDateString()
     @Transform(value => value ? new Date(value) : undefined)
     expiresAt?: Date;
 
+    @ApiPropertyOptional({ name: "max_uses" })
     @ValidateIf((object: CreateSignUpReferenceRequest) => object.maxUses !== null && object.maxUses !== undefined)
     @IsNumber()
     @Expose({name: "max_uses"})
     maxUses?: number;
 
+    @ApiProperty()
     @IsObject()
     @ValidateNested()
     config: CreateSignUpReferenceConfigRequest;
