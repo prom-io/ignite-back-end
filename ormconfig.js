689bbcf2871bb307189d7734c4f88db7c9c74f7f
Edgar <starguitar307@gmail.com>
2020-08-07 03:38:50 +0200

    feat(migrations): use database migrations instad of typeorm synchronization

---
+++ ormconfig.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+require('dotenv').config();
+
+/** @type {import('typeorm').ConnectionOptions} */
+const ormConfig = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  logging: process.env.NODE_ENV === 'development',
+  migrationsTableName: "migrations",
+  migrations: ["dist/migrations/*.js"],
+  cli: {
+    migrationsDir: "src/migrations"
+  },
+};
+
+module.exports = ormConfig;
