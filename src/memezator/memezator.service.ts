171fe79c1d94cae5484d910aa11f73b90844c68f
jonyroww <jonyroww@gmail.com>
2020-08-18 15:12:40 +0300

    feat(memezator):create-winners-post-method

--- src/memezator/memezator.service.ts
+++ src/memezator/memezator.service.ts
@@ -7,7 +7,9 @@ import _ from "lodash"
 import { EtherscanService } from "../etherscan";
 import { StatusLikesRepository } from "../statuses/StatusLikesRepository";
 import { MEMEZATOR_HASHTAG } from "../common/constants"
-import { WinnerMemesWithLikes, MemeWithLikesAndVotingPowers, LikeAndVotingPower } from "./types";
+import { WinnerMemesWithLikes, MemeWithLikesAndVotingPowers, LikeAndVotingPower} from "./types";
+import { UsersRepository } from "src/users";
+import { StatusesService } from "src/statuses";
 
 @Injectable()
 export class MemezatorService {
@@ -15,7 +17,9 @@ export class MemezatorService {
     private readonly statusesRepository: StatusesRepository,
     private readonly hashTagRepository: HashTagsRepository,
     private readonly etherscanService: EtherscanService,
-    private readonly statusLikeRepository: StatusLikesRepository
+    private readonly statusLikeRepository: StatusLikesRepository,
+    private readonly usersRepository: UsersRepository,
+    private statusesService: StatusesService
   ) {}
 
   @Cron(getCronExpressionForMemezatorCompetitionSumminUpCron())
@@ -89,5 +93,65 @@ export class MemezatorService {
     midnightInGreenwich.setUTCHours(0, 0, 0, 0)
 
     return midnightInGreenwich
+  }
+
+  async createWinnersStatus(winnerMemesWithLikes: WinnerMemesWithLikes){
+    const statusUser = await this.usersRepository.findOne({ethereumAddress: process.env.MEME_WINNERS_POST_USER})
+    const firstPlaceAuthor = await this.usersRepository.findOne({id: winnerMemesWithLikes.firstPlace.meme.author.id})
+    const secondPlaceAuthor = await this.usersRepository.findOne({id: winnerMemesWithLikes.secondPlace.meme.author.id})
+    const thirdPlaceAuthor = await this.usersRepository.findOne({id: winnerMemesWithLikes.thirdPlace.meme.author.id})
+
+    const firstPlacePostText = `
+    MEME #1 OF ${new Date(winnerMemesWithLikes.firstPlace.meme.createdAt)}
+
+    Voted: ${winnerMemesWithLikes.firstPlace.likesWithVotingPowers.length} votes
+    Prize: 1100 PROM
+
+    Author: ${firstPlaceAuthor.username} 230 PROM
+    Winner #1: Ivan Ivanov 330 PROM
+    Winner #2: Peter Jones 120 PROM
+    Winner #3: Petr Petroff 100 PROM
+    `
+    const secondPlacePostText = `
+    MEME #1 OF ${new Date(winnerMemesWithLikes.secondPlace.meme.createdAt)}
+
+    Voted: ${winnerMemesWithLikes.secondPlace.likesWithVotingPowers.length} votes
+    Prize: 1100 PROM
+
+    Author: ${secondPlaceAuthor.username} 230 PROM
+    Winner #1: Ivan Ivanov 330 PROM
+    Winner #2: Peter Jones 120 PROM
+    Winner #3: Petr Petroff 100 PROM
+    `
+
+    const thirdPlacePostText = `
+    MEME #1 OF ${new Date(winnerMemesWithLikes.thirdPlace.meme.createdAt)}
+
+    Voted: ${winnerMemesWithLikes.thirdPlace.likesWithVotingPowers.length} votes
+    Prize: 1100 PROM
+
+    Author: ${thirdPlaceAuthor.username} 230 PROM
+    Winner #1: Ivan Ivanov 330 PROM
+    Winner #2: Peter Jones 120 PROM
+    Winner #3: Petr Petroff 100 PROM
+    `
+
+    const firstPlacePost = this.statusesRepository.create()
+    firstPlacePost.author = statusUser
+    firstPlacePost.text = firstPlacePostText
+    firstPlacePost.referredStatus = winnerMemesWithLikes.firstPlace.meme
+
+    const secondPlacePost = this.statusesRepository.create()
+    secondPlacePost.author = statusUser
+    secondPlacePost.text = secondPlacePostText
+    secondPlacePost.referredStatus = winnerMemesWithLikes.secondPlace.meme
+
+    const thirdPlacePost = this.statusesRepository.create()
+    thirdPlacePost.author = statusUser
+    thirdPlacePost.text = thirdPlacePostText
+    thirdPlacePost.referredStatus = winnerMemesWithLikes.thirdPlace.meme
+
+    await this.statusesRepository.save([firstPlacePost, secondPlacePost, thirdPlacePost])
+    return;
   }
 }
