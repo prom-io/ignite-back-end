dcd95d0cdd3a2ad4470eca085b068a678988fc50
Edgar <starguitar307@gmail.com>
2020-08-27 14:34:34 +0200

    fix(statistics-logs): fix optional auth

--- src/statistics-log/statistics-log.controller.ts
+++ src/statistics-log/statistics-log.controller.ts
@@ -2,14 +2,14 @@ import { Controller, Post, UseInterceptors, ClassSerializerInterceptor, Body, Re
 import {Request} from "express";
 import { StatisticsLogService } from "./statistics-log.service";
 import { ApiOkResponse } from "@nestjs/swagger";
-import { AuthGuard } from "@nestjs/passport";
+import { OptionalJwtAuthGuard } from "../jwt-auth/OptionalJwtAuthGuard";
 
 @Controller("/api/v1/statistics-logs")
 export class StatisticsLogController {
     constructor(private statisticsLogService: StatisticsLogService) {}
 
     @UseInterceptors(ClassSerializerInterceptor)
-    @UseGuards(AuthGuard("optionalJwt"))
+    @UseGuards(OptionalJwtAuthGuard)
     @ApiOkResponse()
     @Post()
     public logData(@Body() body: object, @Req() request: Request): void {
