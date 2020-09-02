26b7bd28f5665fcfc77dd86dee22405c742ffe0c
Edgar <starguitar307@gmail.com>
2020-09-02 19:07:24 +0200

    fix(transactions): perform transactions in non-parallel way

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -8,6 +8,7 @@ import { User } from "../users/entities";
 import { TransactionResponse } from "./types/responses/TransactionResponse";
 import { GetTransactionsFilters } from "./types/requests/GetTransactionsFilters";
 import { TransactionMapper } from "./TransactionMapper";
+import { LoggerService } from "nest-logger";
 
 @Injectable()
 export class TransactionsService {
@@ -15,10 +16,24 @@ export class TransactionsService {
     private readonly transactionsRepository: TransactionsRepository,
     private readonly tokenExchangeService: TokenExchangeService,
     private readonly transactionsMapper: TransactionMapper,
+    private readonly logger: LoggerService,
   ) {}
 
   public async performTransactions(transactions: Transaction[]): Promise<Transaction[]> {
-    return Promise.all(transactions.map(transaction => this.performTransaction(transaction)))
+    const completeTransactions: Transaction[] = []
+
+    for (const transaction of transactions) {
+      await this.performTransaction(transaction)
+        .then(resultingTransaction => {
+          transactions.push(resultingTransaction)
+          this.logger.info(`performTransactions: transaction complete: ${JSON.stringify(resultingTransaction)}`)
+        })
+        .catch(err => {
+          this.logger.error(`performTransactions: error occurred: ${JSON.stringify(err)}`)
+        })
+    }
+
+    return completeTransactions
   }
 
   public async performTransaction(transaction: Transaction): Promise<Transaction> {
