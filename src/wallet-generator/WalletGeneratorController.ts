dddd8e87cf3c976979b38eaf73fae680e2f9ffb4
Edgar <starguitar307@gmail.com>
2020-09-16 19:58:04 +0200

    feat(captcha): replace captcha

--- src/wallet-generator/WalletGeneratorController.ts
+++ src/wallet-generator/WalletGeneratorController.ts
@@ -1,8 +1,6 @@
 import {ClassSerializerInterceptor, Controller, Post, UseInterceptors} from "@nestjs/common";
 import {WalletGeneratorApiClient} from "./WalletGeneratorApiClient";
 import {GenerateWalletResponse} from "./types/response";
-import { RateLimit, RateLimiterInterceptor } from "nestjs-rate-limiter";
-import { Recaptcha } from "@nestlab/google-recaptcha";
 
 @Controller("api/v1/wallet")
 export class WalletGeneratorController {
@@ -10,11 +8,6 @@ export class WalletGeneratorController {
 
     }
 
-
-    @UseInterceptors(RateLimiterInterceptor, ClassSerializerInterceptor)
-    // one request per 1h per IP
-    @RateLimit({ points: 1, duration: 60 * 60 })
-    @Recaptcha()
     @UseInterceptors(ClassSerializerInterceptor)
     @Post()
     public generateWallet(): Promise<GenerateWalletResponse> {
