56ff3985a35012f2647f23679f9724c94a33848d
Edgar <starguitar307@gmail.com>
2020-09-16 16:03:15 +0200

    fix: trust proxy & typings

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,17 @@ import {AppModule} from "./AppModule";
 import {config as envConfig} from "./config";
 import { setupSwagger } from "./setup-swagger";
 import { runDbMigrations } from "./run-db-migrations";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
     if (envConfig.AUTO_RUN_DB_MIGRATIONS) {
         await runDbMigrations()
     }
 
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.use(bodyParser.json({limit: Infinity}));
     app.useGlobalPipes(new ValidationPipe({transform: true}));
-    app.set('trust proxy', 1);
+    app.set("trust proxy", 1);
     app.enableCors();
     setupSwagger(app)
     await app.listen(envConfig.IGNITE_API_PORT);
