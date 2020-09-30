624eed82fd566b154e42cbae55589b5ca460634c
jonyroww <jonyroww@gmail.com>
2020-09-30 14:35:39 +0300

    feat(memezator):voting-power-purchase-entity

---
+++ src/memezator/entities/VotingPowerPurchase.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryColumn, CreateDateColumn } from "typeorm";
+
+@Entity({ name: "voting_power_purchase" })
+export class VotingPowerPurchase {
+  @PrimaryColumn()
+  id: string
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @Column({type: 'varchar'})
+  userId: string
+
+  @Column({ type: "uuid" })
+  txnId: string
+
+  @Column({ type: "varchar" })
+  txnHash: string
+
+  @Column({ type: "timestamptz" })
+  txnDate: Date
+
+  @Column({ type: "float" })
+  tokenQnt: number
+
+  @Column({ type: "float" })
+  votingPower: number
+}
---
+++ src/migrations/1601459095132-voting-power-purchase.ts
@@ -0,0 +1,71 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class votingPowerPurchase1601459095132 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({name: 'voting_power_purchase',
+        foreignKeys:[
+            {   
+                name: "FK_voting_power_purchase_user_id",
+                columnNames: ['userId'],
+                referencedTableName: 'user',
+                referencedColumnNames: ['id'],
+                onDelete: 'CASCADE',
+            },
+            {
+                name: "FK_voting_power_purchase_txnId",
+                columnNames:['txnId'],
+                referencedTableName: 'transactions',
+                referencedColumnNames: ['id'],
+                onDelete: 'CASCADE',
+            },
+        ],
+        columns: [
+            {
+                name: "id",
+                type: "uuid",
+                isPrimary: true, 
+            },
+            {
+                name: 'createdAt',
+                type: 'timestamp without time zone',
+                isNullable: false,
+                default: 'NOW()',
+            },
+            {
+                name: "userId",
+                type: "varchar",
+                isNullable: false
+            },
+            {
+                name: "txnId",
+                type: "uuid",
+                isNullable: false
+            },
+            {
+                name: "txnHash",
+                type: "varchar",
+                isNullable: false
+            },
+            {
+                name: "txnDate",
+                type: "timestamptz",
+                isNullable: false,
+            },
+            {
+                name: "tokenQnt",
+                type: "float"
+            },
+            {
+                name: "votingPower",
+                type: "float"
+            }
+
+        ]}))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("voting_power_purchase");
+    }
+
+}
